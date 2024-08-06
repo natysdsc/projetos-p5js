b46b603996fa8d31845593811b8a05252d7dd27f
Nathalia Simões <00001126773372sp@al.educacao.sp.gov.br>
2024-08-06 19:58:56 -0300

    adicionando arquivos js e css

---
+++ style.css
@@ -0,0 +1,20 @@
+posicaoHorizontal =  posicaoHorizontal - 1;
+}
+
+if (mouseX > posicaoHorizontal){
+  posicaoHorizontal =  posicaoHorizontal + 1;
+}
+
+if (mouseY < posicaoVertical){
+  posicaoVertical--;
+}
+  
+if (mouseY > posicaoVertical){
+  posicaoVertical++;
+}
+
+if (mouseIsPressed){
+  cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+}
+  
+}
